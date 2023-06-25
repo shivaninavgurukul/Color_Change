577071c3fe4396519fad7b9edd8441a4e301589c
Shivani Mehta <91535945+shivaninavgurukul@users.noreply.github.com>
2023-06-25 22:33:33 +0530

    Add files via upload

---
+++ color.js
@@ -0,0 +1,24 @@
+var box=document.getElementById("mydiv")
+console.log(box)
+
+var red=document.getElementById("red")
+red.onclick=function(){
+	box.style.backgroundColor="red"
+}
+console.log(red)
+
+var green=document.getElementById("green")
+green.onclick=function(){
+	box.style.backgroundColor="green"
+}
+console.log(green)
+
+
+var yellow=document.getElementById("yellow")
+yellow.onclick=function(){
+	box.style.backgroundColor="yellow"
+}
+console.log(yellow)
+
+
+
